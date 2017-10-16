f78e315d602b15d06c0312b5897a054d17d9aba5
Svetoslava Delcheva <simbelmine.sve@gmail.com>
2017-10-15 17:35:27 -0700

    update: no need of version checking (*the min sdk is supporting it)

--- XYZReader/src/main/java/com/example/xyzreader/utils/DisplayUtils.java
+++ XYZReader/src/main/java/com/example/xyzreader/utils/DisplayUtils.java
@@ -1,6 +1,6 @@
 package com.example.xyzreader.utils;
 
-import android.os.Build;
+import android.app.Activity;
 import android.view.View;
 import android.view.Window;
 
@@ -10,13 +10,11 @@ import android.view.Window;
 
 public class DisplayUtils {
     public static void useImmersiveStickyFullsreenMode(Window window) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.getDecorView().setSystemUiVisibility
-                    (
-                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                                    View.SYSTEM_UI_FLAG_FULLSCREEN |
-                                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    );
-        }
+        window.getDecorView().setSystemUiVisibility
+                (
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                );
     }
 }
