a37f6243ee8721e23ec797f9f05cdf348493605d
Svetoslava Delcheva <simbelmine.sve@gmail.com>
2017-10-22 19:50:08 -0700

    remove: unused code

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
@@ -5,17 +5,12 @@ import android.app.FragmentManager;
 import android.app.LoaderManager;
 import android.content.Loader;
 import android.database.Cursor;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowInsets;
-import android.webkit.WebView;
 
 import com.example.xyzreader.R;
 import com.example.xyzreader.data.ArticleLoader;
@@ -74,31 +69,11 @@ public class ArticleDetailActivity extends AppCompatActivity
     @Override
     public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
         mPagerAdapter.swapCursor(cursor);
-
-//        mCursor = cursor;
-//        mPagerAdapter.notifyDataSetChanged();
-//
-//        // Select the start ID
-//        if (mStartId > 0) {
-//            mCursor.moveToFirst();
-//            // TODO: optimize
-//            while (!mCursor.isAfterLast()) {
-//                if (mCursor.getLong(ArticleLoader.Query._ID) == mStartId) {
-//                    final int position = mCursor.getPosition();
-//                    mPager.setCurrentItem(position, false);
-//                    break;
-//                }
-//                mCursor.moveToNext();
-//            }
-//            mStartId = 0;
-//        }
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> cursorLoader) {
         mPagerAdapter.swapCursor(null);
-//        mCursor = null;
-//        mPagerAdapter.notifyDataSetChanged();
     }
 
     private class MyPagerAdapter extends FragmentStatePagerAdapter {
